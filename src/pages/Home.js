d18c1e1776b3c1cda3d22b06d7174f495e863188
Fabian <fabian@fabz.tv>
2018-03-30 16:57:55 -0300

    globals styles removed , scss is back

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom';
 import VideoHome from '../components/VideoHome';
 import Grid from '../components/Grid';
 
-import globalStyles from '../global-styles';
 
 const AllWork = styled.div`
   padding: 0;
