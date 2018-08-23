b8292cecb133bf4f69036ebccd5093ccd459187a
javier <javier_munoz_navarrete@hotmail.com>
2018-08-23 13:28:23 -0300

    add html new form

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -97,6 +97,8 @@ function isSelectedPageProducts(dataProductsIn) {
     return dataProductsIn.slug === "products";
   }
 
+
+
 const Products = props => (
   <div>
     {console.log("props", props.dataProducts, props.dataProducts[0])}
@@ -125,6 +127,21 @@ const Products = props => (
         </Inpunts>
       </Form>
     </Formulario>
+    <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+    <div className="form-group">
+        <label for="name">Name</label>
+        <input type="text" className="form-control" id="name" />
+    </div>
+    <div className="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+    </div>
+    <div className="form-group">
+        <label for="message">Message</label>
+        <textarea className="form-control" rows="5" id="message"></textarea>
+    </div>
+    <button type="submit" className="btn btn-primary">Submit</button>
+</form>
     
   </div>
 );
